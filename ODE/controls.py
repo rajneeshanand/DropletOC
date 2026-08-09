734912adaca1aa0fc56e9aa76659073d7b28e505
Rajneesh Anand <rajneesh.1491981@gmail.com>
2026-08-08 23:18:54 -0400

    few changes on bc

--- ODE/controls.py
+++ ODE/controls.py
@@ -6,18 +6,15 @@ from scipy.optimize import root_scalar
 from scipy.integrate import cumulative_trapezoid
 
 
-# --------------------------------------------------
 # Parameters
-# --------------------------------------------------
+
 X_T = 0.5
 R_0 = 2.0
 R_T = 2.5
 T   = 1.0
 MU  = 0.5
 
-# --------------------------------------------------
 # Define functions
-# --------------------------------------------------
 def F_s(s):
     val = (33*s**2)/(250*np.sinh(s)**2)
     val *= ((R_T/R_0)*(2*np.cosh(s) - R_T/R_0) - 1)
@@ -28,9 +25,7 @@ target = X_T / R_0
 def root_fun(s):
     return F_s(s) - target
 
-# --------------------------------------------------
 # Root finding (scan initial guesses)
-# --------------------------------------------------
 found = False
 guesses = np.linspace(1, 10, 2000)
 
@@ -49,41 +44,31 @@ if not found:
 
 print(f"solved s = {s:g}")
 
-# --------------------------------------------------
 # Spatial discretization
-# --------------------------------------------------
 Nx = 1200
 x = np.linspace(0, 1, Nx)
 
 rho_x = (R_T*np.sinh(s*x) + R_0*np.sinh(s*(1 - x))) / np.sinh(s)
 
-# --------------------------------------------------
 # Time mapping
-# --------------------------------------------------
 I = np.trapezoid(rho_x**2, x)
 cumulative_integral = cumulative_trapezoid(rho_x**2, x, initial=0)
 
 tau_T = T / (MU * I)
 t_x = T * cumulative_integral / I
 
-# --------------------------------------------------
 # Derivatives
-# --------------------------------------------------
 drho_dx = np.gradient(rho_x, x)
 drho_dtau = drho_dx / tau_T
 
-# --------------------------------------------------
 # Controls
-# --------------------------------------------------
 P_tau = (14/5) * drho_dtau
 p_0 = X_T * 700 / (33 * tau_T)
 
 G0_tau = -(5/6) * P_tau * (rho_x**2)
 delta_G_tau = (11/30) * p_0 * (rho_x**2)
 
-# --------------------------------------------------
 # Plot
-# --------------------------------------------------
 plt.figure(figsize=(9, 6))
 ax = plt.gca()
 
